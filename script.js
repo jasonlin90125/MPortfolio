0118bd1dec6e5038ed0238ce928a9609b5b4ff68
jasonlin90125 <64716220+jasonlin90125@users.noreply.github.com>
2020-11-14 07:46:31 -0700

    Adding Omid's JS code

--- script.js
+++ script.js
@@ -24,3 +24,9 @@ function showDivs(n) {
   x[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " w3-black";
 }
+
+history.scrollRestoration = "manual";
+window.onload = function() {
+  window.scrollTo(0,0);
+  document.querySelector('html').style.scrollBehavior = '';
+}
