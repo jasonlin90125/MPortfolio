d935fefe118f55fcb0d35233c0d5b6c196046ac1
jasonlin90125 <64716220+jasonlin90125@users.noreply.github.com>
2020-11-14 15:11:40 -0700

    Fixing other pages error, credits to Aaron

--- script.js
+++ script.js
@@ -1,29 +1,31 @@
 /* Slideshow stuff */
-var slideIndex = 1;
-showDivs(slideIndex);
+if (window.location.pathname.substring(window.location.pathname.lastIndexOf('/') + 1) == "index.html") {
+  var slideIndex = 1;
+  showDivs(slideIndex);
 
-function plusDivs(n) {
-  showDivs(slideIndex += n);
-}
-
-function currentDiv(n) {
-  showDivs(slideIndex = n);
-}
-
-function showDivs(n) {
-  var i;
-  var x = document.getElementsByClassName("testy");
-  var dots = document.getElementsByClassName("demo");
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length}
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
+  function plusDivs(n) {
+    showDivs(slideIndex += n);
   }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" w3-black", "");
+
+  function currentDiv(n) {
+    showDivs(slideIndex = n);
   }
-  x[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " w3-black";
+
+  function showDivs(n) {
+    var i;
+    var x = document.getElementsByClassName("testy");
+    var dots = document.getElementsByClassName("demo");
+    if (n > x.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = x.length}
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";  
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" w3-black", "");
+    }
+    x[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " w3-black";
+  }
 }
 
 /* Refresh goes back to beginning */
