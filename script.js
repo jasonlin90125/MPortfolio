745b16e322875800ede141aedb15f70566e29318
jasonlin90125 <64716220+jasonlin90125@users.noreply.github.com>
2020-11-15 09:42:54 -0700

    Fourth try

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 /* Slideshow stuff */
 var homePage = Boolean(window.location.pathname.substring(window.location.pathname.lastIndexOf('/') + 1) == "index.html");
-var openPageURL = window.location.protocol + '//' + "jasonlin.netlify.app";
+var openPageURL = "https://jasonlin.netlify.app";
 var openPage = Boolean(window.location.href == openPageURL);
 if (homePage || openPage) {
   var slideIndex = 1;
